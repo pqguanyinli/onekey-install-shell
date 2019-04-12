8846cf10da1a5e955c65ed4356c9e63b0c5ea575
pqguanyinli <34409013+pqguanyinli@users.noreply.github.com>
2019-04-12 17:10:11 +0800

    Update version.sh

--- frps/version.sh
+++ frps/version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export FRPS_VER=0.22.0
+export FRPS_VER=0.26.0
 export FRPS_INIT="https://raw.githubusercontent.com/pqguanyinli/onekey-install-shell/master/frps/frps.init"
 export aliyun_download_url="https://code.aliyun.com/pqguanyinli/frp/raw/master"
 export github_download_url="https://github.com/fatedier/frp/releases/download"
